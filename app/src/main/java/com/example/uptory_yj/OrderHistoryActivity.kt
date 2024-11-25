0c0c5f67292ebf1fdae4e47fbab818f0f9ef451b
yeonjiki <kimberry0708@gmail.com>
2024-11-25 20:50:33 +0900

    first

---
+++ app/src/main/java/com/example/uptory_yj/OrderHistoryActivity.kt
@@ -0,0 +1,4 @@
+package com.example.uptory_yj
+
+class OrderHistoryActivity {
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,11 @@ buildscript {
         google()
         mavenCentral()
     }
+    dependencies {
+        // Gradle Plugin 추가
+        classpath("com.google.gms:google-services:4.3.15") // 최신 버전
+    }
+}
+plugins {
+    alias(libs.plugins.google.gms.google.services) apply false
 }
